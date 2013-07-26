c1165f631f5c5d42bfcfca9e7fa860dece87037e
Alan Storm <astorm@alanstorm.com>
2013-07-26 11:31:28 -0700

    MIT License

--- app/code/local/Alanstormdotcom/Layoutviewer/Model/Observer.php
+++ app/code/local/Alanstormdotcom/Layoutviewer/Model/Observer.php
@@ -1,63 +1,86 @@
 <?php
-	class Alanstormdotcom_Layoutviewer_Model_Observer extends Varien_Object{
-		const FLAG_SHOW_LAYOUT 			= 'showLayout';
-		const FLAG_SHOW_LAYOUT_FORMAT 	= 'showLayoutFormat';		
-		const HTTP_HEADER_TEXT			= 'Content-Type: text/plain';
-		const HTTP_HEADER_HTML			= 'Content-Type: text/html';
-		const HTTP_HEADER_XML			= 'Content-Type: text/xml';
-		
-		private $request;
-		
-		private function init() {
-			$this->setLayout(Mage::app()->getFrontController()->getAction()->getLayout());
-			$this->setUpdate($this->getLayout()->getUpdate());
-		}
-		
-		//entry point
-		public function checkForLayoutDisplayRequest($observer) {			
-			$this->init();
-			$is_set = array_key_exists(self::FLAG_SHOW_LAYOUT, $_GET);
-			if(		$is_set && 'package' == $_GET[self::FLAG_SHOW_LAYOUT]) {
-				$this->outputPackageLayout();
-			}
-			else if($is_set && 'page'    == $_GET[self::FLAG_SHOW_LAYOUT]) {
-				$this->outputPageLayout();			
-			}
-			else if($is_set && 'handles' == $_GET[self::FLAG_SHOW_LAYOUT]) {
-				$this->outputHandles();
-			}
-		}
+/*
+* The MIT License (MIT)
+* 
+* Copyright (c) 2010 - 2013 Pulse Storm LLC
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+class Alanstormdotcom_Layoutviewer_Model_Observer extends Varien_Object{
+    const FLAG_SHOW_LAYOUT 			= 'showLayout';
+    const FLAG_SHOW_LAYOUT_FORMAT 	= 'showLayoutFormat';		
+    const HTTP_HEADER_TEXT			= 'Content-Type: text/plain';
+    const HTTP_HEADER_HTML			= 'Content-Type: text/html';
+    const HTTP_HEADER_XML			= 'Content-Type: text/xml';
+    
+    private $request;
+    
+    private function init() {
+        $this->setLayout(Mage::app()->getFrontController()->getAction()->getLayout());
+        $this->setUpdate($this->getLayout()->getUpdate());
+    }
+    
+    //entry point
+    public function checkForLayoutDisplayRequest($observer) {			
+        $this->init();
+        $is_set = array_key_exists(self::FLAG_SHOW_LAYOUT, $_GET);
+        if(		$is_set && 'package' == $_GET[self::FLAG_SHOW_LAYOUT]) {
+            $this->outputPackageLayout();
+        }
+        else if($is_set && 'page'    == $_GET[self::FLAG_SHOW_LAYOUT]) {
+            $this->outputPageLayout();			
+        }
+        else if($is_set && 'handles' == $_GET[self::FLAG_SHOW_LAYOUT]) {
+            $this->outputHandles();
+        }
+    }
 
-		private function outputHandles() {
-			$update = $this->getUpdate();
-			$handles = $update->getHandles();
-			echo '<h1>','Handles For This Request','</h1>'."\n";
-			echo '<ol>' . "\n";
-			foreach($handles as $handle) {
-				echo '<li>',$handle,'</li>';
-			}
-			echo '</ol>' . "\n";			
-			die();
-		}
-		
-		private function outputHeaders() {
-			$is_set = array_key_exists(self::FLAG_SHOW_LAYOUT_FORMAT,$_GET);			
-			$header		= self::HTTP_HEADER_XML;
-			if($is_set && 'text' == $_GET[self::FLAG_SHOW_LAYOUT_FORMAT]) {
-				$header = self::HTTP_HEADER_TEXT;
-			}
-			header($header);
-		}
-		
-		private function outputPageLayout() {
-			$layout = $this->getLayout();
-			$this->outputHeaders();		
-			die($layout->getNode()->asXML());		
-		}
-		
-		private function outputPackageLayout() {
-			$update = $this->getUpdate();
-			$this->outputHeaders();
-			die($update->getPackageLayout()->asXML());
-		}
-	}
+    private function outputHandles() {
+        $update = $this->getUpdate();
+        $handles = $update->getHandles();
+        echo '<h1>','Handles For This Request','</h1>'."\n";
+        echo '<ol>' . "\n";
+        foreach($handles as $handle) {
+            echo '<li>',$handle,'</li>';
+        }
+        echo '</ol>' . "\n";			
+        die();
+    }
+    
+    private function outputHeaders() {
+        $is_set = array_key_exists(self::FLAG_SHOW_LAYOUT_FORMAT,$_GET);			
+        $header		= self::HTTP_HEADER_XML;
+        if($is_set && 'text' == $_GET[self::FLAG_SHOW_LAYOUT_FORMAT]) {
+            $header = self::HTTP_HEADER_TEXT;
+        }
+        header($header);
+    }
+    
+    private function outputPageLayout() {
+        $layout = $this->getLayout();
+        $this->outputHeaders();		
+        die($layout->getNode()->asXML());		
+    }
+    
+    private function outputPackageLayout() {
+        $update = $this->getUpdate();
+        $this->outputHeaders();
+        die($update->getPackageLayout()->asXML());
+    }
+}
